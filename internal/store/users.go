05b99abf013e3fc2fdc5f6e02973e74a8904b172
Ahmad Ghadiri <ahmad.ghadiri@fireeye.com>
2022-06-09 08:20:38 -0400

    End of part 8

--- internal/store/users.go
+++ internal/store/users.go
@@ -1,10 +1,15 @@
 package store
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 type User struct {
-	Username string `binding:"required,min=5,max=30"`
-	Password string `binding:"required,min=7,max=32"`
+	Username   string `binding:"required,min=5,max=30"`
+	Password   string `binding:"required,min=7,max=32"`
+	CreatedAt  time.Time
+	ModifiedAt time.Time
 }
 
 func AddUser(user *User) error {
